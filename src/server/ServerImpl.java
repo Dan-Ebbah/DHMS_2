37eca139b6d23e8b2c5f7a72f78b51db474ba7ac
Daniel Dan-Ebbah <danielebbah@yahoo.com>
2024-03-01 20:59:17 -0500

    provide logic for operations

--- src/server/ServerImpl.java
+++ src/server/ServerImpl.java
@@ -2,41 +2,76 @@ package server;
 
 import ManagementServer.AppointmentType;
 import ManagementServer.HealthCareSystemPOA;
+import database.HashMapImpl;
+import model.Appointment;
+
+import java.util.List;
 
 public class ServerImpl extends HealthCareSystemPOA {
-    @Override
-    public boolean addAppointment(String appointmentID, AppointmentType appointmentType, int capacity) {
-        return false;
+    private HashMapImpl _database;
+
+    public ServerImpl(HashMapImpl database) {
+        _database = database;
     }
 
     @Override
-    public boolean removeAppointment(String appointmentID, AppointmentType appointmentType) {
-        return false;
+    public String addAppointment(String appointmentID, AppointmentType appointmentType, int capacity) {
+        if (isAppointmentPresent(appointmentID)) {
+            String msg = "Could not add appointment, because appointment seems to already exist";
+//            logger.info(String.format(msg.concat(": Appointment Type = %s, Appointment ID = %s, Appointment Capacity = %d"), appointmentID, appointmentType, capacity));
+            return msg;
+        }
+
+        Appointment appointment = new Appointment(appointmentID, appointmentType, capacity);
+        _database.insert(appointment);
+        String msg = "Successfully added the new appointment";
+        return msg;
     }
 
     @Override
-    public boolean bookAppointment(String patientID, String appointmentType, String appointmentID) {
-        return false;
+    public String removeAppointment(String appointmentID, AppointmentType appointmentType) {
+        if (isAppointmentPresent(appointmentID)) {
+            return _database.remove(new Appointment(appointmentID, appointmentType));
+        }
+        return "Appointment does not exist to be removed";
     }
 
     @Override
-    public boolean cancelAppointment(String patientID, String appointmentType, String appointmentID) {
-        return false;
+    public String bookAppointment(String patientID, AppointmentType appointmentType, String appointmentID) {
+        Appointment appointment = new Appointment(appointmentID, appointmentType);
+        if (isAppointmentPresent(appointmentID)) {
+            return _database.book(patientID, appointment);
+        }
+        return "Couldn't book appointment because Appointment ID is wrong";
+    }
+
+    @Override
+    public String cancelAppointment(String patientID, String appointmentType, String appointmentID) {
+        if (isAppointmentPresent(appointmentID)) {
+            return _database.cancel(patientID, appointmentID);
+        }
+        return "Patient does not have the appointment booked";
     }
 
     @Override
     public String getAppointmentSchedule(String patientID) {
-        return "null";
+        List<Appointment> appointmentList = _database.getByPatientId(patientID);
+        String msg = appointmentList.toString();
+        return msg;
     }
 
     @Override
     public String listAppointmentAvailability(String appointmentType) {
-        return null;
+        return "List ";
     }
 
     @Override
     public boolean swapAppointment(String patientID, String oldAppointmentType, String oldAppointmentID, String newAppointmentType, String newAppointmentID) {
         return false;
+    }
+
+    private boolean isAppointmentPresent(String appointmentID) {
+        return _database.findByAppointmentID(appointmentID) != null;
     }
 
 }
